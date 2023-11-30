0c6e26cb800ef949c0858c5ef7de78609f0b4648
osamablal <osamablal2013@gmail.com>
2023-11-30 11:46:56 -0800

    10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,5 +6,16 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+	size_t depth = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	while (tree->parent != NULL)
+	{
+		depth += 1;
+		tree = tree->parent;
+	}
+
+	return (depth);
 }
