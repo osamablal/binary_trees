9c5670d0a333a367e2485e4b5916f356661cad2f
Osama Bilal <128569407+osamablal@users.noreply.github.com>
2023-11-30 22:10:58 +0200

    Add files via upload

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_right - Rotating The Right binary tree.
+ * @tree: The tree.
+ * Return: New node.
+ */
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	binary_tree_t *pivot;
+
+	if (tree == NULL || tree->left == NULL)
+	{
+		return (NULL);
+	}
+	pivot = tree->left;
+	tree->left = pivot->right;
+	if (pivot->right != NULL)
+	{
+		pivot->right->parent = tree;
+	}
+	pivot->right = tree;
+	pivot->parent = tree->parent;
+	tree->parent = pivot;
+	return (pivot);
+}
