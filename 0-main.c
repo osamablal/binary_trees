c4a078fe5016c3ffa608184efefad431e4f21c2d
osamablal <osamablal2013@gmail.com>
2023-11-30 11:01:58 -0800

    0

--- 0-main.c
+++ 0-main.c
@@ -2,25 +2,24 @@
 #include "binary_trees.h"
 
 /**
- * main - Entry point testing comment
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	binary_tree_t *root;
+    binary_tree_t *root;
 
-	root = binary_tree_node(NULL, 98);
+    root = binary_tree_node(NULL, 98);
 
-	root->lft = binary_tree_node(root, 12);
-	root->lft->lft = binary_tree_node(root->lft, 6);
-	root->lft->rgt = binary_tree_node(root->lft, 16);
+    root->left = binary_tree_node(root, 12);
+    root->left->left = binary_tree_node(root->left, 6);
+    root->left->right = binary_tree_node(root->left, 16);
 
-	root->rgt = binary_tree_node(root, 402);
-	root->rgt->lft = binary_tree_node(root->rgt, 256);
-	root->rgt->rgt = binary_tree_node(root->rgt, 512);
+    root->right = binary_tree_node(root, 402);
+    root->right->left = binary_tree_node(root->right, 256);
+    root->right->right = binary_tree_node(root->right, 512);
 
-	binary_tree_print(root);
-	return (0);
+    binary_tree_print(root);
+    return (0);
 }
-
